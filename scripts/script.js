c69d7fb56c08583d36bbac847aa623526c9cea0a
Igor Khomenko <zmelrosz@yandex.ru>
2022-07-26 13:45:17 +0300

    v2.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,54 @@
 let page = document.querySelector('.page'); //pageObj
 let main = page.querySelector('main'); //mainObj
+
+//explorer
 let explorerEditButton = main.querySelector('.explorer').querySelector('.explorer__person').querySelector('.explorer__description').querySelector('.explorer__title-edit').querySelector('.explorer__edit');
 let explorerTitle = main.querySelector('.explorer').querySelector('.explorer__person').querySelector('.explorer__description').querySelector('.explorer__title-edit').querySelector('.explorer__title');
 let explorerSubtitle = main.querySelector('.explorer').querySelector('.explorer__person').querySelector('.explorer__description').querySelector('.explorer__subtitle'); 
-let popup = page.querySelector('.popup'); //popupObj
-let popupCloseButton = popup.querySelector('.popup__container').querySelector('.popup__close');
-let popupForm = popup.querySelector('.popup__container').querySelector('.popup__form');
-let titleInput = popupForm.querySelector('.popup__input_edit_title');
-let subtitleInput = popupForm.querySelector('.popup__input_edit_subtitle');
-log(titleInput.value);
+
+//popupAdd
+const addCardButton = page.querySelector('.explorer__add');
+const addCardClose = page.querySelector('.popup__close_type_add');
+
+//popup edit vars
+const popupEdit = page.querySelector('.popup_type_edit');
+const popupAdd = page.querySelector('.popup_type_add');
+const popupEditCloseButton = popupEdit.querySelector('.popup__container').querySelector('.popup__close');
+const popupEditForm = popupEdit.querySelector('.popup__container').querySelector('.popup__form');
+let titleInput = popupEditForm.querySelector('.popup__input_edit_title');
+let subtitleInput = popupEditForm.querySelector('.popup__input_edit_subtitle');
+const popupAddForm = page.querySelector('.popup_type_add');
+let popupAddNameInput = popupAddForm.querySelector('.popup__input_edit_name');
+let popupAddUrlInput = popupAddForm.querySelector('.popup__input_edit_url');
 
 
+//cards
+let cardsContainer = main.querySelector('.cards');
 
 // popup events
-function popupToggle()
+function popupEditToggle()
 {
-  popup.classList.toggle('popup_opened');
+  popupEdit.classList.toggle('popup_opened');
 }
-explorerEditButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+
+function popupAddToggle()
+{
+  popupAdd.classList.toggle('popup_opened');
+}
+
+//clickListeners
+explorerEditButton.addEventListener('click', popupEditToggle);
+popupEditCloseButton.addEventListener('click', popupEditToggle);
+addCardButton.addEventListener('click', popupAddToggle);
+addCardClose.addEventListener('click', popupAddToggle);
+
 
 function log(log)
 {
   console.log(log);
 }
 
-function formSubmitHandler (evt) 
+function formSubmitHandler (evt) //popup edit func
 {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                               // Так мы можем определить свою логику отправки.
@@ -44,7 +67,65 @@ function formSubmitHandler (evt)
   subtitleInput.textContent = newSubtitleInputValue;
   explorerTitle.textContent = newTitleInputValue;
   explorerSubtitle.textContent = newSubtitleInputValue;
-  popupToggle();
+  popupEditToggle();
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
+popupEditForm.addEventListener('submit', formSubmitHandler);
+
+
+function addCard(cardName, cardImg)
+{
+  const cardTemp = document.querySelector('#card-temp').content;
+  let cardClone = cardTemp.querySelector('.card').cloneNode(true);
+  
+  cardClone.querySelector('.card__name').textContent = cardName; // set card title
+  cardClone.querySelector('.card__image').src=cardImg; //set img
+
+  cardsContainer.append(cardClone);
+}
+
+function cardToContainer(evt)
+{
+  evt.preventDefault();
+
+  let cardNameInputValue = popupAddNameInput.value;
+  let cardUrlInputValue = popupAddUrlInput.value;
+
+  addCard(cardNameInputValue, cardUrlInputValue);
+  popupAddToggle();
+}
+
+popupAddForm.addEventListener('submit', cardToContainer);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+for (i=0; i < initialCards.length; i++)
+{
+  addCard(initialCards[i].name, initialCards[i].link)
+}
+
